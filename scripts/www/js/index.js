a0ccb5af000c970ac9390ff89b277169f93eb9fb
FP001 <504230031@qq.com>
2025-12-22 17:31:38 +0800

    update src file from pakeplus

---
+++ scripts/www/js/index.js
@@ -0,0 +1,14 @@
+﻿new Vue({
+    el: "#index",
+    data: function () {
+        return {
+        }
+    },
+    methods: {
+        goClick(page) {
+            window.location.href = page;
+        }
+    },
+    mounted: function () {
+    }
+})
